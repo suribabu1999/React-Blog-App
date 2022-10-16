a98d3eaf5764db1e4dcd06495f6324cd45273e2b
Suri1 <sureshtarra1999@gmail.com>
2022-10-16 14:22:28 +0530

    Created custom hook 'useUser' for login

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,1 +1,15 @@
-// const user = useUser();
+import { useState, useEffect } from "react"
+import {getAuth , onAuthStateChanged} from 'firebase/auth'
+
+const useUser = () => {
+
+    const [user , setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(getAuth() , user => {
+            setUser(user);
+            setIsLoading(false);
+        });
+        return unsubscribe;
+    },[])
+}
